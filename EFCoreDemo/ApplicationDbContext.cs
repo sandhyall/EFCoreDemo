35cd20fc4aced3e240bc3efba4155dd4d421922d
sandesh dahal <dsandesh404@gmail.com>
2025-08-07 07:21:11 +0545

    Add project files.

---
+++ EFCoreDemo/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EFCoreDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreDemo
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ EFCoreDemo/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCoreDemo.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string price { get; set; }
+        public string description { get; set; }
+    }
+}
